cd3d26c6fc787ec894a2a8477b4af2e3f6d6e3d4
jasper3745 <jasper528@naver.com>
2025-01-04 11:12:43 +0900

    day1

---
+++ src/day1/Day1Print.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class Day1Print {
+    public static void main(String[] args) {
+        System.out.println("Hello world!!");
+        System.out.print("HEllo world!\n");
+        System.out.println("박재준");
+        System.out.print("중앙중학교\n");
+        System.out.println("좋아하는 과목:과학");
+        System.out.println("싫어하는 과목:국어");
+    }
+}
