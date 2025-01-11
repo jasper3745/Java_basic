39cb20b5f7567c0735b8a73e8bc866449fedf1bd
jasper3745 <jasper528@naver.com>
2025-01-11 11:15:38 +0900

    Day2 change

--- src/day2/Day2Practice.java
+++ src/day2/Day2Practice.java
@@ -7,15 +7,15 @@ public class Day2Practice {
 
         Scanner sc = new Scanner(System.in);
 
-//        System.out.println("출생 연도를 입력하세요: ");
-//        int i1 = sc.nextInt();
-//        System.out.println("당신의 나이는 " + (2025 - i1) + "입니다.");
+        System.out.println("출생 연도를 입력하세요: ");
+        int i1 = sc.nextInt();
+        System.out.println("당신의 나이는 " + (2025 - i1) + "입니다.");
 
-//        System.out.println("사각형의 가로 길이를 입력하세요: ");
-//        double d1 = sc.nextDouble();
-//        System.out.println("사각형의 세로 길이를 입력하세요: ");
-//        double d2 = sc.nextDouble();
-//        System.out.println("사각형의 넓이는 " + (d1 * d2) + " 입니다.");
+        System.out.println("사각형의 가로 길이를 입력하세요: ");
+        double d1 = sc.nextDouble();
+        System.out.println("사각형의 세로 길이를 입력하세요: ");
+        double d2 = sc.nextDouble();
+        System.out.println("사각형의 넓이는 " + (d1 * d2) + " 입니다.");
 
         System.out.println("첫 번째 숫자를 입력하세요: ");
         int i2 = sc.nextInt();
