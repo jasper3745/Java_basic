3b384d28b9652e769f13a1e04e23a529c1634567
jasper3745 <jasper528@naver.com>
2025-02-01 11:22:38 +0900

    day5

---
+++ src/day5/Day5Practice.java
@@ -0,0 +1,11 @@
+package day5;
+
+public class Day5Practice {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i+=2){
+            System.out.println("i = " + i);
+        }
+
+    }
+}
---
+++ src/day5/Day5repeat.java
@@ -0,0 +1,25 @@
+package day5;
+
+public class Day5repeat {
+    public static void main(String[] args) {
+
+//        for (int i = 1; i<= 100; i++) {
+//            System.out.println("i = " + i);
+//        }
+
+//        int count = 1;
+//        while (count <= 5) {
+//            System.out.println("count = " + count);
+//            count++;
+//        }
+
+//        int number = 1;
+//        do {
+//            System.out.println("number = " + number);
+//            number++;
+//        } while (number<=5);
+
+
+
+    }
+}
