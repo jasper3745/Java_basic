602fa079f63813ef087d481d765c91ba4d3e6a4b
jasper3745 <jasper528@naver.com>
2025-02-15 11:24:09 +0900

    day6

---
+++ src/day6/Day6Practice.java
@@ -0,0 +1,76 @@
+package day6;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Day6Practice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+//        for (int i = 1; i <= 10; i++) {
+//            if (i % 2 == 1) {
+//                continue;
+//            }
+//            System.out.println(i);
+//        }
+
+
+//        System.out.println("숫자를 입력하세요: ");
+//        int i = sc.nextInt();
+//        int t = 0;
+//        for(int n = 1; n <= i; n++){
+//            t = n + t;
+//
+//            if (t >= 50) {
+//                System.out.println("합계가 50을 넘었습니다. 반복 종료!");
+//                break;
+//            }
+//            System.out.println("현재 합계: " + t);
+//
+//        }
+//        System.out.println("최종 합계: " + t);
+
+//        System.out.println("삼각형의 높이를 입력하세요: ");
+//        int i = sc.nextInt();
+//        char s = '*';
+//        for(int n = 1; n <= i; n++){
+//            for(int t = 1; t <= n; t++){
+//                System.out.print(s);
+//            }
+//            System.out.println();
+//        }
+
+//        System.out.println("삼각형의 높이를 입력하세요: ");
+//        int i = sc.nextInt();
+//        char st = '*';
+//        char sp = ' ';
+//        int t = 0;
+//        for (int n = 1; n <= i; n++) {
+//            for (int x = i - n; x >= 0; x--) {
+//                System.out.print(sp);
+//            }
+//            for (int t1 = 1; t1 <= n + t; t1++) {
+//                System.out.print(st);
+//            }
+//            System.out.println();
+//            t = t + 1;
+//        }
+
+        //괴생명체
+        int i = sc.nextInt();
+        char st = '*';
+        char sp = ' ';
+        int t = 0;
+        for (int n = 1; n <= i; n++) {
+            for (int x = i - n; x >= 0; x--) {
+                System.out.print(sp);
+                for (int t1 = 1; t1 <= n + t; t1++) {
+                    System.out.print(st);
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+
---
+++ src/day6/Day6deeprepeat.java
@@ -0,0 +1,36 @@
+package day6;
+
+import java.util.Scanner;
+
+public class Day6deeprepeat {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+//        System.out.println("\n숫자 입력(10을 입력하면 종료):");
+//
+//        while (true) {
+//
+//            int number = sc.nextInt();
+//            if (number == 10) {
+//                break;
+//            }
+//            System.out.println("입력한 숫자: " + number);
+//        }
+
+        //continue 사용
+//        for (int i = 1; i <= 10; i++){
+//            if (i == 5){
+//                System.out.println("5 건너뛰기");
+//                continue;
+//            }
+//            System.out.println("i = " + i);
+//        }
+
+//        for(int i = 1; i <= 9; i++) {
+//            for (int j = 2; j <= 9; j++){
+//                System.out.println(j + " x " + i + " = " + (j * i) + "\t");
+//            }
+//            System.out.println();
+//        }
+    }
+}
