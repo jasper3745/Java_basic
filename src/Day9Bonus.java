9f53ddef506fc827620bca6cd9207fa5fdb6ebf2
jasper3745 <jasper528@naver.com>
2025-03-01 11:28:10 +0900

    day9

---
+++ src/Day9Bonus.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Day9Bonus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char st = '*';
+        char sp = ' ';
+        int h = sc.nextInt();
+        for (int n = 1; n <= h; n++) {
+            for(int i = 0; i < n-1; i++)
+                System.out.print(sp);
+            for (int i = 0; i < 2 * h - n ; i++) {
+                System.out.print(st);
+            }
+            System.out.println();
+        }
+    }
+}
