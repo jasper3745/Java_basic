523bbda98b802a6b4c55d5f2e8e2c2ab9bfb3b36
Tuna130411 <eeev.owo@gmail.com>
2025-03-15 11:19:04 +0900

    Day11

---
+++ src/day11/Day11Practice.java
@@ -0,0 +1,27 @@
+package day11;
+
+import java.util.Scanner;
+
+public class Day11Practice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Day11User example = new Day11User();
+
+        System.out.println("아이디를 입력하세요.");
+        example.setId(sc.next());
+        System.out.println("비밀번호를 입력하세요");
+        example.setPassword(sc.next());
+        System.out.println("당신의 아이디: " + example.getId());
+        System.out.println("당신의 비밀번호: " + example.getPassword());
+        System.out.println("으로 가입이 완료되었습니다.");
+        System.out.println("\n비밀번호 변경을 시작 합니다.\n로그인 후 진행 됩니다.");
+        System.out.println("아이디를 입력하세요.");
+        String i = sc.next();
+        System.out.println("비밀번호를 입력하세요");
+        String p = sc.next();
+        if(i.equals(example.getId()) || p.equals(example.getPassword())) {
+            System.out.println("로그인 성공!");
+        }
+
+    }
+}
---
+++ src/day11/Day11User.java
@@ -0,0 +1,23 @@
+package day11;
+
+public class Day11User {
+
+    public String id;
+    public String password;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
