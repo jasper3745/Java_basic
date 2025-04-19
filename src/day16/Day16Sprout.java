b253f197077b188030117e36e38e322f7ad77db2
imhyunn <byuhis938@gmail.com>
2025-04-19 11:25:26 +0900

    구진우

---
+++ src/day16/Day16Sprout.java
@@ -0,0 +1,20 @@
+package day16;
+
+public class Day16Sprout {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+        System.out.println(" ___________            _______________  |                      -------");
+        System.out.println("            |                  /\\        |                    /        \\");
+        System.out.println("            |                 /  \\       |                   /          \\");
+        System.out.println("            |                /    \\      |                   \\          /");
+        System.out.println("            |               /      \\     |                    \\________/");
+        System.out.println("_____________                 |                              ______________");
+        System.out.println("      |                       |                                     |");
+        System.out.println("      |                       |___________                          |");
+    }
+}
